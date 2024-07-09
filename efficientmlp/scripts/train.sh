bcada7b73387457d9c25361e7f6936476d01e9a1
unaG <veraguo11@163.com>
2024-07-10 02:50:34 +0800

    Update train.sh

--- efficientmlp/scripts/train.sh
+++ efficientmlp/scripts/train.sh
@@ -3,7 +3,7 @@ python train.py \
 --compound_coef 1 \
 --load_weights /root/autodl-tmp/efficientdet-d1.pth \
 --num_workers 12 \
---batch_size 1 \
+--batch_size 2 \
 --num_epochs 500 \
 --data_path /root/autodl-tmp/mmlrestroomsign/ \
 --log_path /root/autodl-tmp/logs/ \
