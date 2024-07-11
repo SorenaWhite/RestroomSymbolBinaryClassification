d5d9cceb402f4940580c156999c7c1e6eda9e18d
SorenaWhite <yueanwang@vip.sina.cn>
2024-07-12 04:10:13 +0800

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -16,11 +16,10 @@ model.eval()
 
 def inference(im_pil):
     image_feature = clip_model.encode_image(preprocess(im_pil).unsqueeze(0).to(device))
-    image_tensor = torch.cat([image_feature, image_feature]).float()
+    image_tensor = torch.cat([image_feature]).float()
 
-    male_text_feature = clip_model.encode_text(clip.tokenize("restroom sign of male").to(device))
-    female_text_feature = clip_model.encode_text(clip.tokenize("restroom sign of female").to(device))
-    text_tensor = torch.cat([male_text_feature, female_text_feature]).float()
+    text_feature = clip_model.encode_text(clip.tokenize("toilet sign").to(device))
+    text_tensor = torch.cat([text_feature]).float()
 
     with torch.no_grad():
         print(image_tensor.dtype, text_tensor.dtype)
