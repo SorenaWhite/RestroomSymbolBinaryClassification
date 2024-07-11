75bbd535e4aff628d22b52233a7ad8cb857dc096
unaG <veraguo11@163.com>
2024-07-12 04:18:54 +0800

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -25,8 +25,8 @@ def inference(im_pil):
         print(image_tensor.dtype, text_tensor.dtype)
         preds = model(image_tensor, text_tensor)
         # print(preds)
-        result = preds.topk(1, 1, True, True).cpu().indices[0]
-        
+        result = preds.topk(1, 1, True, True).indices.cpu()[0]
+
     return result
 
 
